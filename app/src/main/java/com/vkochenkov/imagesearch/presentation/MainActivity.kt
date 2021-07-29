3c827731f5ed5d048d74e63ecaa286c10a44a8ac
Vlad Kochenkov <kochenkov1993@gmail.com>
2021-07-29 14:12:59 +0300

    test api call

--- app/src/main/java/com/vkochenkov/imagesearch/presentation/MainActivity.kt
+++ app/src/main/java/com/vkochenkov/imagesearch/presentation/MainActivity.kt
@@ -1,29 +1,60 @@
 package com.vkochenkov.imagesearch.presentation
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.collection.arraySetOf
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.vkochenkov.imagesearch.App
 import com.vkochenkov.imagesearch.R
+import com.vkochenkov.imagesearch.data.api.ApiResponse
+import com.vkochenkov.imagesearch.data.api.ApiService
+import io.reactivex.SingleObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var apiService: ApiService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(arraySetOf(
-                R.id.navigation_home, R.id.navigation_favourites, R.id.navigation_notifications))
+        val appBarConfiguration = AppBarConfiguration(
+            arraySetOf(
+                R.id.navigation_home, R.id.navigation_favourites, R.id.navigation_notifications
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        //todo test api call
+        App.appComponent.inject(this)
+
+        apiService.getAllImages("22696909-4c17dd920fd77d7daf174e4ac", 1)
+            .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(
+                object :
+                    SingleObserver<ApiResponse> {
+                    override fun onSubscribe(d: Disposable) {
+
+                    }
+
+                    override fun onSuccess(t: ApiResponse) {
+
+                    }
+
+                    override fun onError(e: Throwable) {
+
+                    }
+                })
     }
 }
