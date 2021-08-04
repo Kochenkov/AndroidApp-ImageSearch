aa07a1e4934a39e8b9f7855b6dea1368814e255b
Vlad Kochenkov <kochenkov1993@gmail.com>
2021-08-04 11:28:50 +0300

    hide image activity toolbar

--- app/src/main/java/com/vkochenkov/imagesearch/presentation/activity/ImageActivity.kt
+++ app/src/main/java/com/vkochenkov/imagesearch/presentation/activity/ImageActivity.kt
@@ -1,6 +1,7 @@
 package com.vkochenkov.imagesearch.presentation.activity
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -20,14 +21,19 @@ class ImageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image)
 
-        imageView = findViewById(R.id.iv_full_image)
-        progressBar = findViewById(R.id.progress_detail)
-        emptyDataTv = findViewById(R.id.tv_empty_details)
+        initViews()
+        supportActionBar?.hide()
 
         val item = intent.getParcelableExtra<ImageItem>(IMAGE_ITEM)
         val url = item?.largeImageUrl.toString()
         val glideLoader = GlideLoader(this, url, imageView,  emptyDataTv)
         glideLoader.setProgressBar(progressBar)
         glideLoader.loadImage()
+    }
+
+    private fun initViews() {
+        imageView = findViewById(R.id.iv_full_image)
+        progressBar = findViewById(R.id.progress_detail)
+        emptyDataTv = findViewById(R.id.tv_empty_details)
     }
 }
